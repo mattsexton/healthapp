e81e25cad3f1e29802475ed6719a1a76cfc35506
Matt Sexton <mattsexton@me.com>
2014-04-30 18:20:33 -0700

    checkin

--- js/core.js
+++ js/core.js
@@ -1,39 +1,70 @@
-// file:///Users/mattsexton/Sites/index.html
-var client = new Dropbox.Client(
+(function (Dropbox)
 {
-    key: 'lp5b4gpd7jc2z47',
-    secret: 'g8i959uzvy6qfwi',
-    uid: 508841
-});
+    'use strict';
 
-// Try to finish OAuth authorization.
-client.authenticate(
-{
-    interactive: true
-}, function(error, dclient)
-{
-    console.dir(error);
-    console.dir(dclient);
+    var client;
 
-    console.dir(dclient.getUserInfo());
-
-    if (error)
+    function init()
     {
-        alert('Authentication error: ' + error);
+
+        client = new Dropbox.Client(
+        {
+            key: 'lp5b4gpd7jc2z47',
+            secret: 'g8i959uzvy6qfwi',
+            uid: 508841
+        });
+
+        // Try to finish OAuth authorization.
+        client.authenticate(
+        {
+            interactive: false
+        }, function (error)
+        {
+            if (error)
+            {
+                alert('Authentication error: ' + error);
+            }
+        });
+
+        if (client.isAuthenticated())
+        {
+            // Client is authenticated. Display UI.
+            dataStore();
+        }
+        else
+        {
+            console.log('User is not authenticated');
+        }
     }
-    else
+
+    function dataStore()
     {
-        console.log('yay');
+        var dsm = client.getDatastoreManager();
+
+        dsm.openDefaultDatastore(function (error, datastore)
+        {
+            if (error)
+            {
+                alert('Error opening data store: ' + error);
+                return;
+            }
+
+            var healthTable = datastore.getTable('health'),
+                record = healthTable.insert(healthdata[0]);
+
+            console.dir(record.get('date'));
+            console.dir(healthTable.query());
+        });
+
+        dsm.listDatastores(function (error, datastores)
+        {
+            console.dir(datastores);
+        });
+
+        dsm.close();
     }
 
-});
-
-if (client.isAuthenticated())
-{
-    // Client is authenticated. Display UI.
-    console.dir(client);
-}
-else
-{
-    console.log('boo');
-}
+    init();
+}(
+    window.Dropbox
+));
