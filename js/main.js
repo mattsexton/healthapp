c656f88ee314d3b115782e457d7c2dcbed664d3d
Matt Sexton <mattsexton@me.com>
2014-05-05 20:12:29 -0700

    moving things around

--- js/main.js
+++ js/main.js
@@ -1,27 +1,37 @@
-require.config(
+var HealthApp = (function(model, dbc)
 {
-	enforceDefine: true,
-	shim:
-	{
-		dropbox:
-		{
-			exports: 'Dropbox'
-		}
-	},
-	paths:
-	{
-		dropbox: 'https://www.dropbox.com/static/api/dropbox-datastores-1.0-latest'
-	}
-});
+    'use strict';
 
-define(
-	[
-		'dropboxconnector'
-	],
-	function (dbc)
-	{
-		'use strict';
+    dbc.init();
+}(
+	{},
+	HealthApp.dropboxConnector
+));
 
-		dbc.init();
-	}
-);
+// require.config(
+// { 
+// 	enforceDefine: true,
+// 	shim:
+// 	{
+// 		dropbox:
+// 		{
+// 			exports: 'Dropbox'
+// 		}
+// 	},
+// 	paths:
+// 	{
+// 		dropbox: 'https://www.dropbox.com/static/api/dropbox-datastores-1.0-latest'
+// 	}
+// });
+//
+// define(
+// 	[
+// 		'dropboxconnector'
+// 	],
+// 	function (dbc)
+// 	{
+// 		'use strict';
+//
+// 		dbc.init();
+// 	}
+// );
